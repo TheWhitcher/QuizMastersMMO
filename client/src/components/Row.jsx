d45f30ee12259c2295f9a18ca73078fa2fa424ef
ZachW <zwhitcher1995@homtail.com>
2023-09-20 01:21:11 -0400

    Cover your eyes when looking at the span. pretend you didnt see anything

--- client/src/components/Row.jsx
+++ client/src/components/Row.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 
 function Row(props) {
+  const rowStyle = {
+    display: 'flex',
+    height: '30px',
+  };
+
+  const columnStyle = {
+    flex: '1 0 33%',
+    border: '1px solid',
+    padding: '5px',
+  };
+
   return (
     <>
         <div className= {props.style}>
-            <div className="row m-0" style={{height: "30px"}}>
-                <div className="col border">{props.name}</div>
-                <div className="col border">{props.category}</div>
-                <div className="col border">{props.score}</div>
+            <div style={{rowStyle}}>
+            {/*Very ugly solution, but too tired to think of better way*/}
+                <span style={{columnStyle}}>{props.index}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{props.name}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{props.score}</span>
             </div>
         </div>
     </>
   )
 }
+
 
 export default Row
